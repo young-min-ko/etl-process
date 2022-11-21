88fcaaf55516d4b41c615843a6e294ede6148998
Young Min Ko <dudals3394@gmail.com>
2022-11-21 09:43:00 -0500

    version 1.0

--- etl-csv.js
+++ etl-csv.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
+const readFilePath = '/Users/youngminko/hackreactor/full_dataset.csv';
+const writeFilePath= '/Users/youngminko/hackreactor/test4.csv';
 
 
-const readStream = fs.createReadStream('/Users/youngminko/hackreactor/full_dataset.csv', { encoding: 'utf8' })
+const readStream = fs.createReadStream(readFilePath, { encoding: 'utf8' })
   .on('error', (err) => {
     console.log(err);
   })
   .pipe(csv())
 
-  const writeStream = fs.createWriteStream('/Users/youngminko/hackreactor/test4.csv');
+  const writeStream = fs.createWriteStream(writeFilePath);
 
   readStream.on('data', (row)=>{
     console.log(row);
